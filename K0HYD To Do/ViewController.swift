f3b2d20b88da82e984523965bba3b37db2c5cd78
Dale Puckett <dale.puckett@att.net>
2016-11-26 22:57:33 -0600

    Initial Commit

---
+++ K0HYD To Do/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  K0HYD To Do
+//
+//  Created by Dale Puckett on 11/26/16.
+//  Copyright © 2016 k0hyd. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
